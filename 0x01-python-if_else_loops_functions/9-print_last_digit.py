f37a10533cfbf18e89c79b53a28196b08196141c
Michael Appiah <mappiahca@gmail.com>
2023-09-05 23:01:40 -0700

    Fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-    digit = abs(number) % 10
-    print(digit, end = "")
+    ab = number / 10
+    del ab
+    digit = int(str(number)[-1])
+    print(digit, end="")
     return digit
