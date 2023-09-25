041fb47eff78b7c639e914a42453e8c659d07bf6
Michael Appiah <mappiahca@gmail.com>
2023-09-25 09:49:01 -0700

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        print(f"Exception: {e}", file=stderr)
+        return None
