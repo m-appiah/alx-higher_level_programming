8be766d66fccc4179047c4e03336cb0ae5e9e795
Michael Appiah <mappiahca@gmail.com>
2023-09-26 21:15:14 -0700

    Write an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Square class that will be used to instantiate an object"""
+
+
+class Square:
+
+    """An ampty class Square that defines a square.
+    At the moment, it has no attributes and properties.
+    """
+    pass
