f064421600ab17d8dc443a2df225545b3de7a846
Michael Appiah <mappiahca@gmail.com>
2023-10-02 23:32:11 -0700

    String representation - Bug fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Define a class Rectangle"""
+
+
 class Rectangle:
+    """Represent a Rectangle"""
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
