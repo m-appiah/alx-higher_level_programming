b2b9e78fb5dd25df4793c894d79f741df2a88b95
Michael Appiah <mappiahca@gmail.com>
2023-10-05 00:50:20 -0700

    Print square - Bug fix

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -28,5 +28,5 @@ def print_square(size):
 
     for i in range(size):
         for j in range(size):
-            print("#", end=" ")
-        print()
+            print("#", end="")
+        print("")
