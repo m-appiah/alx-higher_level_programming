d6ba6a06580279ba57cb2bc2ea288c973eb03c0d
Michael Appiah <mappiahca@gmail.com>
2023-10-05 07:01:14 -0700

    Max integer - Unittest - bug fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -40,13 +40,12 @@ class TestMaxInteger(unittest.TestCase):
     def test_string(self):
         """Test a string."""
         test_strings = "Michael"
-        self.assertEqual(max_integer(test_strings), 'i')
+        self.assertEqual(max_integer(test_strings), 'k')
 
     def test_empty_string(self):
         """Test an empty string."""
         empty_string = ""
         self.assertEqual(max_integer(empty_string), None)
-
 
     if __name__ == '__main__':
         unittest.main()
