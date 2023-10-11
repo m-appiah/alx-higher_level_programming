df22c0a9a7e09ff734b3d1a9052bb3409f0728ab
Michael Appiah <mappiahca@gmail.com>
2023-10-10 20:26:54 -0700

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,56 +1,49 @@
-import sys
-from collections import defaultdict
-"""A class for computing and printing log metrics"""
+#!/usr/bin/python3
+"""Reads from standard input and computes metrics"""
 
 
-class LogMetrics:
+def print_stats(size, status_codes):
+    """Print accumulated metrics
     """
-    Attributes:
-        total_size (int): Total file size sum.
-        status_code_counts (dict): Counts of each status code.
-        line_number (int): Number of lines processed.
-    """
-    def __init__(self):
-        """Initialize LogMetrics instance."""
-        self.total_size = 0
-        self.status_code_counts = defaultdict(int)
-        self.line_number = 0
-
-    def process_line(self, line):
-        """Process a log line and update metrics."""
-        self.line_number += 1
-
-        parts = line.split()
-        if len(parts) < 10:
-            return
-
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-
-        self.total_size += file_size
-
-        if status_code in {
-                "200", "301", "400", "401",
-                "403", "404", "405", "500"
-                }:
-            self.status_code_counts[status_code] += 1
-
-        if self.line_number % 10 == 0:
-            self.print_statistics()
-
-    def print_statistics(self):
-        """Print computed log metrics."""
-
-        print("Total file size: File size:", self.total_size)
-        for code in sorted(self.status_code_counts.keys()):
-            print(f"{code}: {self.status_code_counts[code]}")
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
 
 if __name__ == "__main__":
-    metrics = LogMetrics()
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
         for line in sys.stdin:
-            metrics.process_line(line)
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        metrics.print_statistics()
+        print_stats(size, status_codes)
+        raise
