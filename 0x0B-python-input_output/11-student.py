f1c3fe30aedf64c54980644220cc964c3475d79c
Michael Appiah <mappiahca@gmail.com>
2023-10-10 19:30:56 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This module defines a class Student"""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieve a JSON representation of the object.
+
+        Args:
+            attrs: A list of attribute names for JSON representation.
+
+        Returns:
+            dict: A dictionary representing the object's attributes.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student"""
+        for k, v in json.items():
+            setattr(self, k, v)
