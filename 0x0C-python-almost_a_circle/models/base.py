88d2da5ae74305dda15b1e221505abb290bb79af
Michael Appiah <mappiahca@gmail.com>
2023-10-16 20:38:24 -0700

    base bug fix and Square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Base class for managing unique identifiers in other classes"""
+import json
 
 
 class Base:
@@ -16,3 +17,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert a list of dictionaries to a JSON string."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
