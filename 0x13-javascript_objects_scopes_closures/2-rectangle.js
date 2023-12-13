2b093b605a563c739a2afe682d9e969a62f6f2e0
Michael Appiah <mappiahca@gmail.com>
2023-12-12 18:26:53 -0800

    2. Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a class Rectangle that defines a rectangle
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
