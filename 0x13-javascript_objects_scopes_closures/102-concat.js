c4a40f6270126fa1ffd546279fe4394295f3ac54
Michael Appiah <mappiahca@gmail.com>
2023-12-13 04:00:59 -0800

    Bug fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,11 +2,7 @@
 // Write a script that concats 2 files.
 
 const fs = require('fs');
+const contentA = fs.readFileSync(process.argv[2], 'utf8');
+const contentB = fs.readFileSync(process.argv[3], 'utf8');
 
-const [, , fileA, fileB, fileC] = process.argv;
-
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
-const contentC = contentA + '\n' + contentB;
-
-fs.writeFileSync(fileC, contentC);
+fs.writeFileSync(process.argv[4], contentA + contentB);
