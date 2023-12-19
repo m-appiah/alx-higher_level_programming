b5a6aec66f9fa96cf2d45e5efe6353ec8e87f7cd
Michael Appiah <mappiahca@gmail.com>
2023-12-19 15:39:25 -0800

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Changes the name of a State object from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    ask = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name)
+    engine = create_engine(ask)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
