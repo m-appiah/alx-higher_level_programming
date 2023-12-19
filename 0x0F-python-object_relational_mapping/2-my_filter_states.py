c24586fa9538a4c5f40c8277358e52566a489f4c
Michael Appiah <mappiahca@gmail.com>
2023-12-19 09:24:06 -0800

    Filter states by user input - bug fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,9 +22,8 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute(
-            "SELECT id, name FROM states WHERE name = '{0}' ORDER BY id".formt(
-                argv[4]))
+    cursor.execute('SELECT id, name FROM states WHERE name = "{0}" '
+                   'ORDER BY id'.format(argv[4]))
     rows = cursor.fetchall()
 
     for row in rows:
