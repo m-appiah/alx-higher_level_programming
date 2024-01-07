302d8cfa241a94e313f435126de5a2a7acd55bd9
Michael Appiah <mappiahca@gmail.com>
2024-01-07 00:12:16 -0800

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""sends a POST request to the passed URL with the email"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data=data, method='POST')
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read().decode('utf-8')
+        print(content)
