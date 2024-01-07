95949241ff44faea99cd6d06cbdb4cd6ca21f48a
Michael Appiah <mappiahca@gmail.com>
2024-01-07 02:14:04 -0800

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""request to the URL and displays the body of the response (decoded in utf-8)"""
+import urllib.request
+import urllib.error
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as r:
+            content = r.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
