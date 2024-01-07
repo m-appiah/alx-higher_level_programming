26f7a89fae702cc1211b86e08e3602b812984bde
Michael Appiah <mappiahca@gmail.com>
2024-01-07 02:58:26 -0800

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """email address, sends a POST request to the passed URL"""
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
 
-    email = {'email': argv[2]}
-    r = requests.post(argv[1], data=email)
+    email = {'email':sys.argv[2]}
+    r = requests.post(sys.argv[1], data=email)
 
     print("Your email is: {}".format(r.text)
