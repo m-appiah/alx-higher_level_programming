d7bb92e7391e47d05dc922f0e4348dd4c6a145b4
Michael Appiah <mappiahca@gmail.com>
2024-01-07 02:21:54 -0800

    What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status"""
+import requests
+
+
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
