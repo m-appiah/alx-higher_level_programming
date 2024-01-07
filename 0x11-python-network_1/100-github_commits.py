c4b658caffdfe742f5c58df4446cba6744dfea33
Michael Appiah <mappiahca@gmail.com>
2024-01-07 04:50:24 -0800

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,12 +2,12 @@
 """list 10 commits (from the most recent to oldest) of the
 repository “rails” by the user “rails”
 """
-from sys import argv
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
     response = requests.get(url)
     commits = response.json()
     try:
