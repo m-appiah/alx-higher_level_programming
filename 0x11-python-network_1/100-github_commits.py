3be241c3fff90e233ba51231104bb33d3f48b258
Michael Appiah <mappiahca@gmail.com>
2024-01-07 04:30:55 -0800

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,13 +2,13 @@
 """list 10 commits (from the most recent to oldest) of the
 repository “rails” by the user “rails”
 """
-from sys import argv
+import sys
 import requests
 
 
 if __name__ == "__main__":
-    repository_name = argv[1]
-    owner_name = argv[2]
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
     url = "https://api.github.com/repos/{}/{}/commits".format(
             owner_name, repository_name)
