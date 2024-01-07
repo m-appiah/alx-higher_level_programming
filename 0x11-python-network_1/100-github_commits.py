9cafacbd9d087ce92fb9490593147299abd43862
Michael Appiah <mappiahca@gmail.com>
2024-01-07 04:47:17 -0800

    Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,24 +2,18 @@
 """list 10 commits (from the most recent to oldest) of the
 repository “rails” by the user “rails”
 """
-import sys
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-            owner_name, repository_name)
-    pars = {"per_page": 10}
-
-    response = requests.get(url, pars=pars)
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    response = requests.get(url)
     commits = response.json()
     try:
-        for commit in commits:
-            sha = commit.get("sha")
-            author_name = commit.get("commit").get("author").get("name")
-            print("{}: {}".format(sha, author_name))
+        for commit in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"), commits[i].gt(
+                    "commit").get("author").get("name")))
     except IndexError:
         pass
