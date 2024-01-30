30c386b65d627b1bae966e2206ab8206c171d041
Michael Appiah <mappiahca@gmail.com>
2024-01-30 09:16:30 -0800

    Star wars movie title and Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// The shebang line specifies that this script should be executed using the Node.js interpreter
+
+const request = require('request');
+// Import the 'request' module to simplify HTTP requests
+const movieId = process.argv[2];
+// Get the movie ID from the command line arguments
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+// Construct the API URL based on the provided movie ID
+
+request.get(url, (error, response, body) => {
+	// Make a GET request to the specified URL
+	if (error) {
+		// Handle errors
+		console.log(error);
+	} else {
+		// Parse the JSON response
+		const data = JSON.parse(body);
+
+		// Print the title of the movie
+		console.log(data.title);
+	}
+});
