0d87aa108c38089bfb515fe1f22e6b62d0c0fa19
Michael Appiah <mappiahca@gmail.com>
2024-01-30 18:09:18 -0800

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-// script should be executed using the Node.js interpreter
+// The script should be executed using the Node.js interpreter
 
 const request = require('request');
 // Import the 'request' module to simplify HTTP requests
 
 const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
-let chars = [];
+let characters = [];
 // Initialize an array to store character URLs
 
 request(url, (error, response, body) => {
@@ -21,7 +21,7 @@ request(url, (error, response, body) => {
   const data = JSON.parse(body);
 
   // Extract character URLs from the movie data
-  chars = data.chars;
+  characters = data.characters;
 
   // Call the getCharacters function to start fetching and printing characters
   getCharacters(0);
@@ -30,12 +30,12 @@ request(url, (error, response, body) => {
 // Recursive function to fetch and print characters
 const getCharacters = (index) => {
   // Base case: Stop recursion when the index reaches the end of the character list
-  if (index === chars.length) {
+  if (index === characters.length) {
     return;
   }
 
   // Make a GET request to each character URL
-  request(chars[index], (error, response, body) => {
+  request(characters[index], (error, response, body) => {
     if (error) {
       // Handle errors during character request
       console.log(error);
@@ -43,10 +43,10 @@ const getCharacters = (index) => {
     }
 
     // Parse the JSON response for each character
-    const charData = JSON.parse(body);
+    const characterData = JSON.parse(body);
 
     // Print the name of each character
-    console.log(charData.name);
+    console.log(characterData.name);
 
     // Recursively call the function to fetch and print the next character
     getCharacters(index + 1);
