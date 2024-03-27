eb2f51972d83f275dc1600765da748e51bb6536b
Michael Appiah <mappiahca@gmail.com>
2024-03-27 01:51:54 -0700

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+// The shebang line specifies that this script should be executed using the Node.js interpreter
+
+const request = require('request');
+// Import the 'request' module to simplify HTTP requests
+const url = process.argv[2];
+// Get the URL from the command line arguments
+const characterId = '18';
+let count = 0;
+// Make a GET request to the specified URL and Handle errors
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // Parse the JSON response
+    const data = JSON.parse(body);
+
+    // Loop through the films and characters to count appearances of the specified character
+    data.results.forEach((film) => {
+      film.characters.forEach((character) => {
+        if (character.includes(characterId)) {
+          count += 1;
+        }
+      });
+    });
+
+    // Print the count
+    console.log(count);
+  }
+});
